a542d1f5697479c57a323ecce5191851952bdfac
UdayGattu <udaygattu007@gmail.com>
2022-10-25 04:16:02 +0530

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,21 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>UdayGattu's Portfolio</title>
+      </Head>
+      {/* Header */}
+      {/* Hero */}
+      {/* about */}
+      {/* experience */}
+      {/* skills */}
+      {/* projects */}
+      {/* contact me */}
+    </div>
+  );
+};
+
+export default Home;
