d9e7392cf1bab7ff416c1de3197f9f3fb26b314c
UdayGattu <udaygattu007@gmail.com>
2022-10-26 15:36:32 +0530

    Projects

---
+++ components/ContactMe.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+function contactMe({}: Props) {
+  return <div>contactMe</div>;
+}
+
+export default contactMe;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import About from "../components/About";
+import ContactMe from "../components/ContactMe";
 import Experience from "../components/Experience";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
+import Projects from "../components/Projects";
 import Skills from "../components/Skills";
 
 const Home: NextPage = () => {
@@ -25,8 +27,14 @@ const Home: NextPage = () => {
       <section id="skills" className="snap-start">
         <Skills />
       </section>
-      {/* projects */}
+      <section id="projects" className="snap-start">
+        <Projects />
+      </section>
+
       {/* contact me */}
+      <section id="contact">
+        <ContactMe />
+      </section>
     </div>
   );
 };
